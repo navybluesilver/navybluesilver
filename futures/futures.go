021cb60fefb422df948929adcb2f81ed612184e0
navybluesilver <navybluesilver@protonmail.ch>
2018-07-30 09:21:25 +0700

    Adding XAU

--- futures/futures.go
+++ futures/futures.go
@@ -11,6 +11,11 @@ const (
 	future2 = "Z18"
 )
 
+
+type BitmexLastPriceResponse []struct {
+	LastPrice                      float64         `json:"lastPrice"`
+}
+
 type Tickers []struct {
 	Name         string  `json:"name"`
 	Description  string  `json:"description"`
@@ -18,57 +23,49 @@ type Tickers []struct {
 	CurrentValue float64 `json:"currentValue"`
 }
 
-type Futures struct {
-	Futures            []Future
+type Products struct {
+	Tickers            []Product
 	Future1Description string
 	Future2Description string
 }
 
-type Future struct {
+type Product struct {
 	Underlying string  `json:"underlying"`
 	SPOT       float64 `json:"SPOT"`
 	Future1    float64 `json:"Future1"`
 	Future2    float64 `json:"Future2"`
 }
 
-func LoadFutures() (f Futures) {
+func GetAllProducts() (p Products) {
 	t, err := getTickers()
 	handleError(err)
 
-	f.Futures = append(f.Futures, load(t, "USD"))
-	f.Futures = append(f.Futures, load(t, "ADA"))
-	f.Futures = append(f.Futures, load(t, "BCH"))
-	f.Futures = append(f.Futures, load(t, "EOS"))
-	f.Futures = append(f.Futures, load(t, "ETH"))
-	f.Futures = append(f.Futures, load(t, "LTC"))
-	f.Futures = append(f.Futures, load(t, "TRX"))
-	f.Futures = append(f.Futures, load(t, "XRP"))
-	f.Future1Description = future1
-	f.Future2Description = future2
-	fmt.Printf("%v\n", f)
-	return f
+	p.Tickers = append(p.Tickers, getProduct(t, "USD"))
+	p.Tickers = append(p.Tickers, getProduct(t, "ADA"))
+	p.Tickers = append(p.Tickers, getProduct(t, "BCH"))
+	p.Tickers = append(p.Tickers, getProduct(t, "EOS"))
+	p.Tickers = append(p.Tickers, getProduct(t, "ETH"))
+	p.Tickers = append(p.Tickers, getProduct(t, "LTC"))
+	p.Tickers = append(p.Tickers, getProduct(t, "TRX"))
+	p.Tickers = append(p.Tickers, getProduct(t, "XRP"))
+	p.Tickers = append(p.Tickers, getProduct(t, "XAU"))
+	p.Future1Description = future1
+	p.Future2Description = future2
+	fmt.Printf("%v\n", p)
+	return p
 }
 
-func load(t Tickers, u string) (f Future) {
-	f.Underlying = u
-	f.SPOT = loadTicker(t, u)
-	f.Future1 = loadFuture(t, u, future1)
-	f.Future2 = loadFuture(t, u, future2)
-	return f
+func getProduct(t Tickers, u string) (p Product) {
+	p.Underlying = u
+	p.SPOT = getTicker(t, u)
+	p.Future1 = getFuture(t, u, future1)
+	p.Future2 = getFuture(t, u, future2)
+	return p
 }
 
-func loadTicker(all Tickers, u string) (value float64) {
-	for _, t := range all {
-		if t.Name == u {
-			return t.CurrentValue
-		}
-	}
-	return 0
-}
-
-func loadFuture(all Tickers, u string, future string) (value float64) {
+func getFuture(all Tickers, u string, future string) (value float64) {
 	name := fmt.Sprintf("%s%s", u, future)
-	return loadTicker(all, name)
+	return getTicker(all, name)
 }
 
 func handleError(err error) {
@@ -97,4 +94,13 @@ func getTickers() (t Tickers, err error) {
 	}
 
 	return t, nil
+}
+
+func getTicker(all Tickers, u string) (value float64) {
+	for _, t := range all {
+		if t.Name == u {
+			return t.CurrentValue
+		}
+	}
+	return 0
 }
